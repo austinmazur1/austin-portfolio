82785d83203fd0282013365f2fad65fd70ab1e45
Austin Mazur <mazuraustin1@gmail.com>
2024-01-29 20:56:46 -0500

    Add photo and change name styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,54 @@
 import Image from "next/image";
+import Photo from "@/public/images/Austin_4.jpg";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Austin Mazur
-        </p>
+        <div className="flex flex-col items-start ">
+          <p
+            //  className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
+            className="fixed left-0 top-0 text-4xl flex w-full justify-center  pt-8 lg:static lg:w-auto"
+          >
+            Austin Mazur
+          </p>
+          <p
+            //  className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
+            className="fixed left-0 top-0 text-xl flex w-full justify-center pb-6 lg:static lg:w-auto  lg:rounded-xl "
+          >
+            Full Stack Web Developer
+          </p>
+        </div>
+
         <div className="fixed bottom-0 gap-10 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a 
-          href="#about"
-          className="hover:text-slate-600">About</a>
-          <a href="#projects"
-          className="hover:text-slate-600">Projects</a>
-          <a  href="#contact"
-          className="hover:text-slate-600">Contact</a>
+          <a href="#about" className="hover:text-slate-600">
+            About
+          </a>
+          <a href="#projects" className="hover:text-slate-600">
+            Projects
+          </a>
+          <a href="#contact" className="hover:text-slate-600">
+            Contact
+          </a>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        {/* <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        /> */}
+      <div className="flex -space-x-5 items-start ">
+        <div className="w-96 h-60 bg-blue-900 rounded-lg items-center">
+          About text goes here
+        </div>
+        <div className="flex justify-center items-center w-60 h-72 rounded-full border-8 overflow-hidden">
+          <Image
+            // className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
+            className="object-cover"
+            src={Photo}
+            alt="Avatar"
+            width={240}
+            height={240}
+            priority
+          />
+        </div>
       </div>
-
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
