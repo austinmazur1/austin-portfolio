5369f5c61f32ac62770c21c02a5f9d0342707ce6
Austin Mazur <mazuraustin1@gmail.com>
2024-01-30 20:42:17 -0500

    Change layout of landing, start adding content to about section

--- app/page.tsx
+++ app/page.tsx
@@ -3,121 +3,79 @@ import Photo from "@/public/images/Austin_4.jpg";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <div className="flex flex-col items-start ">
-          <p
-            //  className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
-            className="fixed left-0 top-0 text-4xl flex w-full justify-center  pt-8 lg:static lg:w-auto"
-          >
-            Austin Mazur
-          </p>
-          <p
-            //  className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
-            className="fixed left-0 top-0 text-xl flex w-full justify-center pb-6 lg:static lg:w-auto  lg:rounded-xl "
-          >
-            Full Stack Web Developer
-          </p>
+    <>
+      <main className="flex min-h-screen flex-col items-center p-10">
+        <div className="h-screen flex flex-col w-full">
+          <div className="z-10  w-full items-center justify-end font-mono text-sm lg:flex">
+            <div className="fixed bottom-0 gap-10 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+              <a href="#about" className="hover:text-slate-600">
+                About
+              </a>
+              <a href="#projects" className="hover:text-slate-600">
+                Projects
+              </a>
+              <a href="#contact" className="hover:text-slate-600">
+                Contact
+              </a>
+            </div>
+          </div>
+
+          <div className="flex flex-col items-center justify-center w-full h-screen">
+            <p>Hey there!</p>
+            {/* TODO on mount animation of a icon or something that appears from behind the developer text */}
+            <p>I'm Austin and I am a Full Stack Developer</p>
+            {/* BMX rider */}
+            {/* martial artist */}
+            {/* Idea do a animation where it changes the text and with each text we have an avatar or image */}
+          </div>
         </div>
 
-        <div className="fixed bottom-0 gap-10 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a href="#about" className="hover:text-slate-600">
-            About
-          </a>
-          <a href="#projects" className="hover:text-slate-600">
-            Projects
-          </a>
-          <a href="#contact" className="hover:text-slate-600">
-            Contact
-          </a>
+        <div className="w-full h-screen flex flex-col items-center pt-12">
+          <p id="about" className="">
+            About me
+          </p>
+          <div className="flex justify-evenly pt-24 items-center">
+            <div className="flex w-60 h-72 rounded-full border-8 overflow-hidden">
+              <Image
+                className="object-cover"
+                src={Photo}
+                alt="Avatar"
+                width={240}
+                height={240}
+                priority
+              />
+            </div>
+            <p className="w-1/2">
+              I'm a full-stack developer with experience in React, ExpressJS, NextJS, Tailwind, React Native and Firebase. 
+              I am a Long Island New York native but after a few years traveling I eventually settled down in
+              Spain.
+              <br/>
+              <br/>
+              I have always
+              been an avid learner, eager to soak up as much knowledge as I can
+              and get to experimenting and getting creative in any way I can. I
+              remember the second I pieced together some HTML and CSS and saw
+              the results in my browser I was hooked!
+              <br/>
+              <br/>
+              I
+              am a BMX rider, a martial artist, a coffee enthusiast, all outlets
+              to express myself. What I love about programming is it scratches
+              that same itch. The possibilities are endless and with a bit of
+              patience, hard work and eagerness to learn and grow the necessary
+              skills, there's no telling what we can build.
+            </p>
+          </div>
         </div>
-      </div>
 
-      <div className="flex -space-x-5 items-start ">
-        <div className="w-96 h-60 bg-blue-900 rounded-lg items-center">
-          About text goes here
+        <div className="w-full h-screen flex flex-col items-center pt-12">
+          <p id="projects" className="">Projects</p>
         </div>
-        <div className="flex justify-center items-center w-60 h-72 rounded-full border-8 overflow-hidden">
-          <Image
-            // className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
-            className="object-cover"
-            src={Photo}
-            alt="Avatar"
-            width={240}
-            height={240}
-            priority
-          />
+
+        <div className="w-full h-screen flex flex-col items-center pt-12">
+          <p id="contact" className="">Contact</p>
         </div>
-      </div>
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
