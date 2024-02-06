e7f9078e635613b527e5559cff0a863b0e733d4d
Austin Mazur <mazuraustin1@gmail.com>
2024-02-05 22:56:31 -0500

    Start projects layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Photo from "@/public/images/Austin_4.jpg";
+import styles from "@/app/test.module.css";
 
 export default function Home() {
   return (
@@ -11,9 +12,11 @@ export default function Home() {
               <a href="#about" className="hover:text-slate-600">
                 About
               </a>
+              <div className={styles.test} />
               <a href="#projects" className="hover:text-slate-600">
                 Projects
               </a>
+              <div className={styles.test2} />
               <a href="#contact" className="hover:text-slate-600">
                 Contact
               </a>
@@ -30,7 +33,7 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="w-full h-screen flex flex-col items-center pt-12">
+        <div className="w-full h-screen flex flex-col items-center pt-12 justify-center">
           <p id="about" className="">
             About me
           </p>
@@ -46,34 +49,44 @@ export default function Home() {
               />
             </div>
             <p className="w-1/2">
-              I'm a full-stack developer with experience in React, ExpressJS, NextJS, Tailwind, React Native and Firebase. 
-              I am a Long Island New York native but after a few years traveling I eventually settled down in
-              Spain.
-              <br/>
-              <br/>
-              I have always
-              been an avid learner, eager to soak up as much knowledge as I can
-              and get to experimenting and getting creative in any way I can. I
-              remember the second I pieced together some HTML and CSS and saw
-              the results in my browser I was hooked!
-              <br/>
-              <br/>
-              I
-              am a BMX rider, a martial artist, a coffee enthusiast, all outlets
-              to express myself. What I love about programming is it scratches
-              that same itch. The possibilities are endless and with a bit of
-              patience, hard work and eagerness to learn and grow the necessary
-              skills, there's no telling what we can build.
+              I'm a full-stack developer with experience in React, ExpressJS,
+              NextJS, Tailwind, React Native and Firebase. I am a Long Island
+              New York native but after a few years traveling I eventually
+              settled down in Spain.
+              <br />
+              <br />
+              I have always been an avid learner, eager to soak up as much
+              knowledge as I can and get to experimenting and getting creative
+              in any way I can. I remember the second I pieced together some
+              HTML and CSS and saw the results in my browser I was hooked!
+              <br />
+              <br />I am a BMX rider, a martial artist, a coffee enthusiast, all
+              outlets to express myself. What I love about programming is it
+              scratches that same itch. The possibilities are endless and with a
+              bit of patience, hard work and eagerness to learn and grow the
+              necessary skills, there's no telling what we can build.
             </p>
           </div>
         </div>
 
-        <div className="w-full h-screen flex flex-col items-center pt-12">
-          <p id="projects" className="">Projects</p>
+        <div className="w-full h-screen flex flex-col items-center pt-12 ">
+          <p id="projects" className="">
+            Projects
+          </p>
+          <div className="flex w-full justify-center gap-4 h-screen pt-12 pb-12">
+            <div className="bg-blue-500 w-1/3 h-96">proj1</div>
+            <div className="bg-blue-800 w-1/3">Description</div>
+          </div>
+          <div className="flex w-full justify-center gap-4 h-screen mb-12">
+            <div className="bg-blue-500 w-1/3 h-96">proj1</div>
+            <div className="bg-blue-800 w-1/3">Description</div>
+          </div>
         </div>
 
-        <div className="w-full h-screen flex flex-col items-center pt-12">
-          <p id="contact" className="">Contact</p>
+        <div className="w-full h-screen flex flex-col items-center mt-12">
+          <p id="contact" className="">
+            Contact
+          </p>
         </div>
       </main>
     </>
