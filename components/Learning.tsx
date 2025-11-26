0012eeafc7a7b458f71bc4bd475cd5a00951b220
Austin Mazur <mazuraustin1@gmail.com>
2025-11-26 16:42:43 +0100

    Add new learning entry
    - Add a new entry for the Full-Stack Web Development Bootcamp at Ironhack to the learnings data.

--- components/Learning.tsx
+++ components/Learning.tsx
@@ -4,7 +4,7 @@ import {
     CardDescription,
     CardHeader,
     CardTitle,
-    CardFooter
+    CardFooter,
   } from "@/components/ui/card";
   import { learnings } from "@/data/learnings";
   import { ExternalLink } from "lucide-react";
@@ -15,7 +15,8 @@ import {
       <section id="learning" className="py-24">
         <h2 className="mb-12 text-3xl font-bold tracking-tight">Education & Learning</h2>
         <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
-          {learnings.map((learning, index) => (
+          {learnings.map((learning, index) => {
+            return (
             <Card key={index} className="border-card bg-secondary/20 shadow-none">
               <CardHeader>
                 <div className="flex items-start justify-between gap-2">
@@ -28,16 +29,15 @@ import {
                   </span>
                 </div>
               </CardHeader>
-              <CardContent>
-             
-              </CardContent>
+              {/* <CardContent>
+              </CardContent> */}
               <CardFooter>
                 <Link href={learning.link} target="_blank" className="cursor-pointer flex items-center gap-2 text-sm text-muted-foreground hover:text-primary">
                   <ExternalLink /> View Certificate
                 </Link>
               </CardFooter>
             </Card>
-          ))}
+          )})}
         </div>
       </section>
     );
--- data/learnings.ts
+++ data/learnings.ts
@@ -11,5 +11,11 @@ export const learnings: Learning[] = [
         description: "Udemy course to prepare for the AWS Certified Cloud Practitioner exam.",
         date: "2025",
         link: "https://www.udemy.com/certificate/UC-80e59d30-2ba1-4310-aeb9-09851cb2fd2d/",
+    },
+    {
+        title: "Full-Stack Web Development Bootcamp at Ironhack",
+        description: "Full-stack web development bootcamp focused on building real-world projects using modern technologies.",
+        date: "2023",
+        link: '/certificates/ironhack-certificate.png',
     }
 ];
