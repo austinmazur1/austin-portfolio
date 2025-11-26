2edddddb49947828489f65b4ecd193f0e5981068
Austin Mazur <mazuraustin1@gmail.com>
2025-11-26 16:12:18 +0100

    Add Learning section and data
    - Introduce Learning component to display education and learning information.
    - Create learnings data model. section.

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import About from "@/components/About";
 import Experience from "@/components/Experience";
 import Projects from "@/components/Projects";
 import Contact from "@/components/Contact";
+import Learning from "@/components/Learning";
 
 export default function Home() {
   return (
@@ -19,6 +20,9 @@ export default function Home() {
       </SectionWrapper>
       <SectionWrapper>
         <Projects />
+      </SectionWrapper>
+      <SectionWrapper>
+        <Learning />
       </SectionWrapper>
       <SectionWrapper>
         <Contact />
---
+++ data/learnings.ts
@@ -0,0 +1,15 @@
+export interface Learning {
+    title: string;
+    description: string;
+    date: string;
+    link: string;
+}
+
+export const learnings: Learning[] = [
+    {
+        title: "Ultimate AWS Certified Cloud Practitioner CLF-C02",
+        description: "Udemy course to prepare for the AWS Certified Cloud Practitioner exam.",
+        date: "2025",
+        link: "https://www.udemy.com/certificate/UC-80e59d30-2ba1-4310-aeb9-09851cb2fd2d/",
+    }
+];
